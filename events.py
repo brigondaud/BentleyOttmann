44740d6dfecde823a42c9e6bf81a2ec5acc05e62
Yann Burrer <yann.burrer@ensimag.grenoble-inp.fr>
2017-04-01 15:28:03 +0200

    creation of events file

---
+++ events.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env pytnon3
+"""
+Events module
+contains the Event object and the Events container.
+"""
+
+from sortedcontainers import SortedList
+
+class Event:
+
+    def __init__(self, type):
+        #TODO: definition of the class.
+
+
+class Events:
+
+    def __init__(self):
+        #TODO: definition of the class.
