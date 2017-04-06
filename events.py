75106c51abf7390c37b40853e37a58f4901b19db
Baptiste Rigondaud <baptiste.rigondaud@grenoble-inp.org>
2017-04-06 10:42:24 +0200

    Init segment and test init

--- events.py
+++ events.py
@@ -1,10 +1,12 @@
-#! /usr/bin/env pytnon3
+#! /usr/bin/env python3
 """
 Events module
 contains the Event object and the Events container.
 """
 
-from sortedcontainers import SortedList
+from sortedcontainers import SortedListWithKey
+from geo.point import Point
+from geo.segment import Segment
 
 CREATION = 0
 DESTRUCTION = 1
@@ -17,25 +19,21 @@ class Event:
     in order to sort it in the Events structure.
     """
 
-    def __init__(self, event_type, coordinates):
+    def __init__(self, event_type, point):
         """
         event_type is the type of the event (creation, destruction or intersections),
         coordinates, the coordinates of the Point, and segments a list of
         the segments associated to the point.
         """
-        #TODO: definition of the class.
-        pass
         self.type = event_type
-        self.key = coordinates
+        self.key = point
         self.segments = []
 
     def event_comparison(self):
         """
         used as a key to compare the elements beetween them.
         """
-        return (- self.key[1], - self.key[0])
-
-
+        return (-self.key.coordinates[1], -self.key.coordinates[0])
 
 class Events:
     """
@@ -45,4 +43,39 @@ class Events:
 
     def __init__(self):
         #TODO: compute the optimal load number
-        self.event_list = SortedListWithKey(None, Event.event_comparison())
+        self.event_list = SortedListWithKey(None, Event.event_comparison)
+
+    def init_segment_events(self, segment):
+        """
+        creates two event for the segment
+        """
+        event_creation = Event(CREATION, segment.endpoints[0])
+        event_destruction = Event(DESTRUCTION, segment.endpoints[1])
+        # Add the segment to both events
+        event_creation.segments.append(segment)
+        event_destruction.segments.append(segment)
+        # Add the two events in the events structure
+        self.event_list.add(event_creation)
+        self.event_list.add(event_destruction)
+
+    def __str__(self):
+        """
+        returns the events from the event list (debugg)
+        """
+        return " | ".join([str(event.key) for event in iter(self.event_list)])
+
+def events_init_test():
+    """
+    test the init of a segment in the series of event
+    """
+    print("------------Segment init test------------")
+    events = Events()
+    events.init_segment_events(Segment([Point([1.0, 2.0]), Point([3.0, 4.0])]))
+    print(events)
+    print("-----------------------------------------")
+
+if __name__ == "__main__":
+    """
+    run the tests sequence
+    """
+    events_init_test()
