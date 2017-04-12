6eefb181b97df22d73304b25bc388c6e68b1ed66
Baptiste Rigondaud <baptiste.rigondaud@grenoble-inp.org>
2017-04-12 10:54:19 +0200

    Begging sweep line fixme and continue

--- bo.py
+++ bo.py
@@ -8,10 +8,14 @@ for each file:
     - print some statistics
 """
 import sys
+from sortedcontainers import SortedListWithKey
 from geo.segment import load_segments
 from geo.tycat import tycat
 from events import Events
-from segmenttree import SegmentTree
+
+CREATION = 0
+DESTRUCTION = 1
+INTERSECTION = 2
 
 def bentley_ottmann(segments, adujster):
     """
@@ -21,14 +25,28 @@ def bentley_ottmann(segments, adujster):
     """
     # adds all the creation and destruction events for the given segments
     events = Events(segments)
-    # creates the structure for the 'alive' segments
-    living_segments = SegmentTree()
+    # creates the structure for the 'alive' segments:
+    # the structure contains a list with the segment and it's key
+    #TODO: compute the optimal load for living segments
+    living_segments = SortedListWithKey(None, lambda segment_tuple: segment_tuple[1])
 
     while not events.isempty():
         # getting the first event in the events list
         current_event = events.event_list.pop(0)
+        if current_event.type == CREATION:
+            current_segment = current_event.segments[0]
+            #the key corresponds to an endpoint of a living segment
+            living_segments.add([current_segment, segment_key_compute(current_event, current_segment)])
 
     return events
+
+def segment_key_compute(event, segment):
+    """
+    computes the key = [abscissa, angle] based on the current event ordinate
+    and the segment.
+    """
+    # sweep_line = Segment(Point([]))
+    #TODO: compute segment keg
 
 def test(filename):
     """
