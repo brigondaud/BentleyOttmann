9b73c98246b15911a9d55fa0f2fc78863c7cb34e
Baptiste Rigondaud <baptiste.rigondaud@grenoble-inp.org>
2017-04-24 18:41:05 +0200

    Iterating on neighbours

--- events.py
+++ events.py
@@ -146,11 +146,22 @@ def intersect_with(event, segment, living_segments, adjuster):
     """
     # Searching for the current segment in the living segments
     # and searching for its nearest neighbours
-    #TODO
+    for neighbour in neighbours(segment, living_segments):
+        pass
     if event.type == INTERSECTION:
         # Can produce intersection that already exists
         pass
     # yields on ((point1, segment1), (point2, segment2),...)
+
+def neighbours(segment, segments):
+    """
+    yields the neighbour segments of segment in segments
+    """
+    segment_index = segments.index(segment)
+    if segment_index > 0:
+        yield segments[segment_index - 1]
+    if segment_index < len(segments)-1:
+        yield segments[segment_index + 1]
 
 def events_init_test():
     """
